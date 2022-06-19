cb5e4e5accbfd90724a5766b1a40b8b43ecb38f4
@guilhermeLouzavio <guilherme.louzavio@gmail.com>
2022-06-19 02:16:42 -0300

    first version project

---
+++ ConsumeRabbit/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+namespace ConsumeRabbit
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+                factory.UserName = "name";
+                factory.Password = "password";
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "GuiApp",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                     Console.WriteLine(" [x] Received {0}", message);
+                     channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch(Exception)
+                    {
+                        //o ideal é fazer essa tratativa caso houver falha de conexao 
+                        //ou algo der errado ao consumir a fila
+                        //voce devolve o erro para fila 
+                        //o ultimo parametro significa retentativa
+                       channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
+
+                channel.BasicConsume(queue: "GuiApp",
+                                    autoAck: false,
+                                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ProjetoRabbitMQ/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Specialized;
+using System.Threading;
+using System;
+using RabbitMQ.Client;
+using System.Text;
+
+
+namespace ProjetoRabbitMQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           string queue = ""; 
+           string msg = ""; 
+         Console.WriteLine("Testando o envio de mensagens para uma Fila do RabbitMQ");
+         Console.WriteLine("Coloque o nome da fila para onde ser enviada a mensagem");
+
+        queue =  Console.ReadLine();
+
+
+           var factory = new ConnectionFactory() { HostName = "localhost" };
+               factory.UserName = "guilrez";
+                factory.Password = "Guilherme99";
+                using(var connection = factory.CreateConnection())
+                using(var channel = connection.CreateModel())
+                {
+                    queue = String.IsNullOrEmpty(queue) ? "FilaPadrao" : queue;
+                    channel.QueueDeclare(queue: queue,
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                    
+                    Console.WriteLine($"Fila {queue} Criada");
+                    Console.WriteLine($"Digite a mensagem a ser enviada ");
+                   
+                    msg =  Console.ReadLine();
+
+                    int count = 0;
+                    while(true){                  
+                        msg = count.ToString();
+                        var body = Encoding.UTF8.GetBytes(msg);
+
+                        channel.BasicPublish(exchange: "",
+                                            routingKey: queue,
+                                            basicProperties: null,
+                                            body: body);
+                        Console.WriteLine(" [x] Sent {0}", msg);
+                        Thread.Sleep(200); 
+
+                        //Console.WriteLine($"Gostaria de enviar uma nova mensagem?"); 
+                        //msg =  Console.ReadLine();
+                        count++;                  
+                    }
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();        
+        }
+    }
+}
